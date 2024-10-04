cf8ff3222d7dab39de8749320ed1effb63894db2
1100062079 <1100062079@L815WD220004.edu.cbe.ab.ca>
2024-10-04 11:20:57 -0600

    11:20 AM

---
+++ Chapter11/src/skillbuilders/myFile.java
@@ -0,0 +1,33 @@
+package skillbuilders;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class myFile {
+
+	public static void main(String[] args) {
+
+		File textFile;
+		Scanner input = new Scanner(System.in);
+		String filename;
+		
+		
+		//Obtain file name from the user
+		System.out.println("Please enter a file name: ");
+		filename = input.nextLine();
+		
+		//Determine if a file exists
+		textFile = new File(filename);
+		
+		if(textFile.exists()) 
+		{
+			System.out.println("File exists.");
+		}
+		else
+		{
+			System.out.println("File does not exist.");
+		}
+		
+	}
+
+}
