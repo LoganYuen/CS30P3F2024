5c856618798b640820e3482441e03ed268570984
logan <logan@DESKTOP-H3SEM05>
2024-11-04 21:04:47 -0700

    9:04PM

--- Chapter 8/src/University/University.java
+++ Chapter 8/src/University/University.java
@@ -1,9 +1,24 @@
 package University;
 
+import java.util.Scanner;
+
+import PressMark.Employee;
+
 public class University {
 	
 	public static void main(String[] args) 
 	{
+		Staff staffMem = new Staff("Bob", "Kid", 60000, "Sec");
+		Faculty facultyMem = new Faculty("Jack", "brown", 100000, "Professer");
+		Employee emp;
+		String action;
+		int empNum;
+		double payArg;
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("");
 		
 	}
 }
